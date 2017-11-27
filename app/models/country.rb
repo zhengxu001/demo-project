2209ea5dacb0e53f9c8e0e8adfcf9cef90c7bd6a
Zen <zhengxu5511@gmail.com>
2017-11-27 21:30:52 +0000

    remove useless model

--- app/models/country.rb
+++
@@ -1,9 +0,0 @@
-class Country
-  property :label, :predicate => RDF::RDFS.label, :type => String
-  property :type, predicate: RDF::RDFV.type
-  property :default_geometry, predicate: RDF::URI.new("http://schemas.opengis.net/geosparql/1.0/geosparql_vocab_all.rdf#defaultGeometry")
-
-  def initialize(paramas)
-  end
-end
-
